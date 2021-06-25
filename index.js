b8aa584dbaa97907364dc8eff09bbcd3ee3723af
AndrewArloro <86027030+AndrewArloro@users.noreply.github.com>
2021-06-25 18:47:43 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
-drawName(Welcome!);
-bouncebubbles();
+red = [0, 100, 63];
+green = [87, 98, 28];
+
+letterColors = [red, green, white];
+
+message = 'Welcome!';
+
+drawName(message, letterColors);
+bounceBubbles();
