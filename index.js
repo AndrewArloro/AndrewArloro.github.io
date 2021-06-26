ec7b3477e33686a6dd548102911761e7266707a2
AndrewArloro <86027030+AndrewArloro@users.noreply.github.com>
2021-06-25 20:51:35 -0400

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ green = [87, 98, 28];
 
 letterColors = [red, green, white];
 
+mouseResponseThreshold = 500
+friction = 0.85
+rotationForce = 0.015
+
 message = 'Welcome!';
 
 drawName(message, letterColors);
