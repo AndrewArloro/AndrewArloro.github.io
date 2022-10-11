8a6c3f5a8170af43dc6aba7742033b1dd959ae18
AndrewArloro <86027030+AndrewArloro@users.noreply.github.com>
2022-10-11 19:12:14 -0400

    Update index.js

--- index.js
+++ index.js
@@ -4,21 +4,10 @@ green = [87, 98, 28];
 letterColors = [red, green, white];
 
 mouseResponseThreshold = 500
-friction = 0.75
+friction = 0.55
 rotationForce = 0.500
 
 message = 'Welcome!';
 
 drawName(message, letterColors);
 bounceBubbles();
-
-function open(sectionName) {
-  // Define variables
-  var i;
-  var x = document.getElementsByClassName("tabcont");
-  
-  for (i = 0; i < x.length; i++) {
-    x[i] = style.display = "none";
-  }
-  document.getElementById(sectionName).style.display = "block";
-}
