60ec2f32674fdf4711249fc93e944ccc220b4683
AndrewArloro <86027030+AndrewArloro@users.noreply.github.com>
2023-01-06 01:24:47 -0500

    Benvenuto!'

--- index.js
+++ index.js
@@ -1,17 +1,3 @@
-function openCity(evt, cityName) {
-  var i, tabcontent, tablinks;
-  tabcontent = document.getElementsByClassName("tabcontent");
-  for (i = 0; i < tabcontent.length; i++) {
-    tabcontent[i].style.display = "none";
-  }
-  tablinks = document.getElementsByClassName("tablinks");
-  for (i = 0; i < tablinks.length; i++) {
-    tablinks[i].className = tablinks[i].className.replace(" active", "");
-  }
-  document.getElementById(cityName).style.display = "block";
-  evt.currentTarget.className += " active";
-}
-
 red = [0, 100, 63];
 green = [87, 98, 28];
 
@@ -21,7 +7,7 @@ mouseResponseThreshold = 500
 friction = 0.55
 rotationForce = 0.500
 
-message = 'Welcome!';
+message = 'Benvenuto!';
 
 drawName(message, letterColors);
 bounceBubbles();
